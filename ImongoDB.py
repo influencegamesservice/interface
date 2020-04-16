80f1a67735db9dcb2316dda354ff9948357f02fe
root <influencegamesservice@gmail.com>
2020-04-16 22:16:28 +0900

    ImongoDB更新

--- ImongoDB.py
+++ ImongoDB.py
@@ -2,7 +2,33 @@ from pymongo import MongoClient
 from abc import ABCMeta, abstractmethod
 import mainsetting
 
-class Abstractmongodb(metaclass=ABCMeta):
+class ABSBaseMongoDB(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.DB_of_Twitch = MongoClient()['Twitch']
+        self.Collection_of_Common = self.DB_of_Twitch.Common
+        self.Collection_of_Games = self.DB_of_Twitch.Games
+        self.Collection_of_Streamers = self.DB_of_Twitch.Streamers
+    
+    @staticmethod
+    def ALLDOCUMENTclear(collection):
+        collection.delete_many({})
+    
+    @staticmethod
+    def DOCUMENTinsert(collection, docs):
+        collection.insert_one(docs)
+    
+    @staticmethod
+    def DOCUMENTupdate_many(collection, filter, update):
+        collection.update_many(filter, update)
+    
+    @staticmethod
+    def ALLDOCUMENTprint(collection):
+        documents = collection.find({})
+        for d in documents:
+            print(d)
+
+class Abstractmongodb(ABSBaseMongoDB):
 
     @abstractmethod
     def start(self):
@@ -23,21 +49,3 @@ class Abstractmongodb(metaclass=ABCMeta):
     @abstractmethod
     def set_streamer_info(self, streamer_json):
         pass
-
-    @staticmethod
-    def ALLDOCUMENTclear(collection):
-        collection.delete_many({})
-    
-    @staticmethod
-    def DOCUMENTinsert(collection, docs):
-        collection.insert_one(docs)
-    
-    @staticmethod
-    def DOCUMENTupdate_many(collection, filter, update):
-        collection.update_many(filter, update)
-    
-    @staticmethod
-    def ALLDOCUMENTprint(collection):
-        documents = collection.find({})
-        for d in documents:
-            print(d)
