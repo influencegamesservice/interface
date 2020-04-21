051dc68be3c1ffc5a9951da7df2370d187d9278e
int <influencegamesservice@gmail.com>
2020-04-21 15:37:54 +0900

    更新

--- ImongoDB.py
+++ ImongoDB.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 class ABSBaseMongoDB(metaclass=ABCMeta):
 
     def __init__(self):
-        self.DB_of_Twitch = MongoClient()['Twitch']
+        self.DB_of_Twitch = MongoClient(connect=False)['Twitch']
         self.Collection_of_Common = self.DB_of_Twitch.Common
         self.Collection_of_Games = self.DB_of_Twitch.Games
         self.Collection_of_Streamers = self.DB_of_Twitch.Streamers
@@ -23,7 +23,11 @@ class ABSBaseMongoDB(metaclass=ABCMeta):
     
     @staticmethod
     def ALLDOCUMENTreturn(collection):
-        return collection.find({})
+        documets = collection.find({})
+        list_documet = []
+        for d in documets:
+            list_documet.append(d)
+        return list_documet
     
     @staticmethod
     def ALLDOCUMENTprint(collection):
@@ -36,7 +40,6 @@ class ABSGet_Twitch_Data_From_MongoDB(ABSBaseMongoDB):
     def __init__(self):
         super().__init__()
     
-
     @abstractmethod
     def get_all_game_data(self):
         pass
