35143fb01465a435af2d318bb60455d8954dee8e
int <influencegamesservice@gmail.com>
2020-04-22 16:15:17 +0900

    更新

--- IcssDB.py
+++ IcssDB.py
@@ -1,1 +1,24 @@
 from abc import ABCMeta, abstractmethod
+
+class ABStwitchCssDataBase(metaclass=ABCMeta):
+    """twitch用のCSSDataBaseの抽象クラス"""
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        """start関数は常に依存関係が無く、いつでもどこでも呼べる"""
+        pass
+
+    @abstractmethod
+    def check_twitch_game_data_is_new(self, game_data) -> bool:
+        pass
+
+    @abstractmethod
+    def insert_twitch_game_data(self, game_data):
+        pass
+
+    @abstractmethod
+    def update_twitch_game_data(self, game_data):
+        pass
